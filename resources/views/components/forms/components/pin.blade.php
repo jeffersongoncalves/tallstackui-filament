fed1714cb2796145b9dca43c47590de9f34430a6
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2025-03-27 20:26:20 -0300

    feat(forms): add Pin component for form inputs

---
+++ resources/views/components/forms/components/pin.blade.php
@@ -0,0 +1,33 @@
+@php
+    $statePath = $getStatePath();
+@endphp
+
+<x-dynamic-component
+    :component="$getFieldWrapperView()"
+    :field="$field"
+    :prefix-icon="false"
+    :suffix-icon="false"
+    :password="false"
+>
+    <x-dynamic-component :component="TallStackUi::prefix('pin')"
+        :invalidate="true"
+        :clear="$getClearable()"
+        :length="$getLength()"
+        :letters="$getLetters() ? true : null"
+        :numbers="$getNumbers() ? true : null"
+        :prefix="$getPrefix()"
+        :attributes="
+                $attributes
+                    ->merge([
+                        'autofocus' => $isAutofocused(),
+                        'disabled' => $isDisabled(),
+                        'id' => $getId(),
+                        'required' => $isRequired() && (! $isConcealed()),
+                        'wire:loading.attr' => 'disabled',
+                        $applyStateBindingModifiers('wire:model') => $statePath,
+                    ], escape: false)
+                    ->merge($getExtraAttributes(), escape: false)
+                    ->merge($getExtraInputAttributes(), escape: false)
+            "
+    />
+</x-dynamic-component>
---
+++ src/Forms/Components/Pin.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace TallStackUIFilament\TallStackUIFilament\Forms\Components;
+
+use Closure;
+use Filament\Forms\Components\Concerns\HasExtraInputAttributes;
+use Filament\Forms\Components\Field;
+use TallStackUIFilament\TallStackUIFilament\Concerns\CanBeClearable;
+use TallStackUIFilament\TallStackUIFilament\Enums\MaskType;
+
+class Pin extends Field
+{
+    use HasExtraInputAttributes;
+    use CanBeClearable;
+
+    protected string $view = 'tallstackuifilament::components.forms.components.pin';
+
+    protected Closure | int $length = 5;
+
+    protected Closure | string | null $prefix = null;
+
+    protected Closure | bool $letters = false;
+
+    protected Closure | bool $numbers = false;
+
+    public function length(Closure | int $length): static
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function prefix(Closure | string $prefix): static
+    {
+        $this->prefix = $prefix;
+
+        return $this;
+    }
+
+    public function letters(Closure | bool $condition = true): static
+    {
+        $this->letters = $condition;
+
+        return $this;
+    }
+
+    public function numbers(Closure | bool $condition = true): static
+    {
+        $this->numbers = $condition;
+
+        return $this;
+    }
+
+    public function getLength(): int
+    {
+        return $this->evaluate($this->length);
+    }
+
+    public function getPrefix(): ?string
+    {
+        return $this->evaluate($this->prefix);
+    }
+
+    public function getLetters(): bool
+    {
+        return $this->evaluate($this->letters);
+    }
+
+    public function getNumbers(): bool
+    {
+        return $this->evaluate($this->numbers);
+    }
+}
